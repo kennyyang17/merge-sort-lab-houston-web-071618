2060f9cff63946decd5b58eed7c4a74611262465
Kenny Yang <kennyyang19@gmail.com>
2019-07-13 06:48:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 
+function findMinAndRemoveSorted(array) {
+  return array.shift()
+}
+
+function merge(first, second) {
+  let sorted = [];
+  while (first.length !== 0 && second.length !== 0) {
+    if (first[0] < second[0]) {
+      sorted.push(findMinAndRemoveSorted(first))
+    } else {
+      sorted.push(findMinAndRemoveSorted(second))
+    }
+  }
+  return sorted.concat(first).concat(second);
+}
+
+function mergeSort(array) {
+  let midpoint = array.length/2
+  let firstHalf = array.slice(0, midpoint)
+  let secondHalf = array.slice(midpoint, array.length)
+  let sorted;
+  if (array.length < 2) {
+    return array;
+  } else {
+    sorted = merge(mergeSort(firstHalf), mergeSort(secondHalf));
+  }
+  return sorted;
+}
+
+// function mergeSort(array){
+//   let midpoint = array.length/2
+//   let firstHalf = array.slice(0, midpoint)
+//   let secondHalf = array.slice(midpoint, array.length)
+//   let sorted;
+
+//   if(array.length < 2){
+//     return array
+//   } else {
+//     sorted = merge(mergeSort(firstHalf), mergeSort(secondHalf))
+//   }
+//   return sorted
+// }
